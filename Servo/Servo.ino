3934a762cdd3d9e79f5af018cf2d942af3d33aff
fatgenius <dbtlive@gmail.com>
2018-11-05 15:06:38 +0800

    转向舵机
    左右摇摆180度

---
+++ Servo/Servo.ino
@@ -0,0 +1,30 @@
+/* This is the servo part 
+--- Author: Butian Du
+--- Time: 10/26/2018
+--- project: robot for cutting grass 
+--- Servo part for the robort 
+--- using the servo.h library 
+*/
+
+#include  <Servo.h>
+
+Servo jsd_servo; //create an object for servo object
+int pos = 0;
+
+void setup() {
+  jsd_servo.attach(9);//
+}
+
+void loop() {
+  for(int pos =0; pos<=180; pos++)
+    {
+      jsd_servo.write(pos);
+      delay(10);
+    }
+
+   for(int pos= 180; pos>=0; pos--)
+    {
+      jsd_servo.write(pos);
+      delay(10);
+    }
+}
